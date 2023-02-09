ebc698115a64eae4a5b281f62c0e4f9bea1f1435
Dustin Lee <leedustin@live.com>
2023-02-08 21:39:31 -0500

    back end code added

--- server/models/index.js
+++ server/models/index.js
@@ -1,3 +1,3 @@
-const Profile = require('./Profile');
+const Shirt = require('./Shirt');
 
-module.exports = { Profile };
+module.exports = { Shirt };
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,22 +1,17 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-  type Profile {
+  type Shirt {
     _id: ID
-    name: String
-    skills: [String]!
+    itemName: String
+    size: String
+    inStock: Boolean
+    category: String
   }
 
   type Query {
-    profiles: [Profile]!
-    profile(profileId: ID!): Profile
-  }
-
-  type Mutation {
-    addProfile(name: String!): Profile
-    addSkill(profileId: ID!, skill: String!): Profile
-    removeProfile(profileId: ID!): Profile
-    removeSkill(profileId: ID!, skill: String!): Profile
+    findAllShirts: [Shirt]!
+    findOneShirt(shirtId: ID): Shirt
   }
 `;
 
