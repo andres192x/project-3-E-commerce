cd8571ddfc2abf9b0a456331a46226eb8398773d
EkamjotSidhu <ekamsidhu5678@gmail.com>
2023-02-13 21:00:35 -0500

    Display shirts component

---
+++ client/src/pages/DisplayShirts.js
@@ -0,0 +1,37 @@
+import React from 'react';
+// Import the `useQuery()` hook from Apollo Client
+import { useQuery } from '@apollo/client';
+
+import ShirtList from '../components/Shirts';
+
+// Import the query we are going to execute from its file
+import { QUERY_SHIRTS } from '../utils/queries';
+
+const DisplayShirts = () => {
+    // Execute the query on component load
+    const { loading, data } = useQuery(QUERY_SHIRTS);
+
+    // Use optional chaining to check if data exists and if it has a thoughts property. If not, return an empty array to use.
+    const shirts = data?.findAllShirts || [];
+console.log(data)
+console.log(shirts)
+    return (
+        <main>
+            <div className="flex-row justify-center">
+                <div className="col-12 col-md-8 mb-3">
+                    {/* If the data is still loading, render a loading message */}
+                    {loading ? (
+                        <div>Loading...</div>
+                    ) : (
+                        <ShirtList
+                            shirts={shirts}
+                            title="All shirts"
+                        />
+                    )}
+                </div>
+            </div>
+        </main>
+    );
+};
+
+export default DisplayShirts;
